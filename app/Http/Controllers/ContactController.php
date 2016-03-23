bd170fb091802ec7759b4f685a4112a8c23f3be7
BentabetAbdelDjalil <bentabetj@yahoo.fr>
2016-03-23 20:57:40 +0100

    controller finalisé marche trés bien

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,12 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
-
+use Mail;
 use App\Http\Requests;
 
 class ContactController extends Controller
 {
-    
+
+	public function getinfos(){
+
+     return view('form.contact');
+
+	}
+
+    public function postinfos(ContactRequest $request){
+       
+     Mail::send('form.admin', $request ->all(), function($message){
+              $message->to('bentabetj@yahoo.fr')->subject('new message');
+
+        });
+
+     return view('form.confirm');
+   
+           
+    }
 }

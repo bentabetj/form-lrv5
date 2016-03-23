83d4025a0a4cab4229bec2effcaf4e3e6189e62d
BentabetAbdelDjalil <bentabetj@yahoo.fr>
2016-03-23 21:08:59 +0100

    légère modification design

--- resources/views/form/contact.blade.php
+++ resources/views/form/contact.blade.php
@@ -2,16 +2,16 @@
 
 @section('contenu')
 
-<form action="" method="POST" role="form">
+<form action="http://localhost/form-lrv5/public/contact" method="POST" role="form">
 	<legend>Form title</legend>
 
 	<div class="form-group">
 		<label for="nom">Nom</label></br>
 		<input type="text" name="nom" class="form-control" id="nom">
-		<label for="">Mail</label></br>
-		<input type="mail" name="email" class="form-control" id="nom"></br>
+		<label for="email">Mail</label></br>
+		<input type="mail" name="email" class="form-control" id="email"></br>
 		<label for="inputTexte">Message</label>
-		<textarea name="message" id="inputTexte" class="form-control" rows="3">
+		<textarea name="texte" id="inputTexte" class="form-control" rows="3">
 		</textarea>
 	</div>
 

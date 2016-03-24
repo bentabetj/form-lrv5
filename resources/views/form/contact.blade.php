0d0e98341f8397a11c7bd71417a5b582c226e309
BentabetAbdelDjalil <bentabetj@yahoo.fr>
2016-03-24 18:28:00 +0100

    la validation mise en place avec  , ça marche pas

--- resources/views/form/contact.blade.php
+++ resources/views/form/contact.blade.php
@@ -5,15 +5,17 @@
 <form action="http://localhost/form-lrv5/public/contact" method="POST" role="form">
 	<legend>Form title</legend>
 
-	<div class="form-group">
+	<div class="form-group {{ $errors->has('nom') ? 'has-error' : '' }}">
 		<label for="nom">Nom</label></br>
+		{{ $errors->first('nom', '<small class="help-block">:message</small>') }}
+	</div>	
 		<input type="text" name="nom" class="form-control" id="nom">
 		<label for="email">Mail</label></br>
 		<input type="mail" name="email" class="form-control" id="email"></br>
 		<label for="inputTexte">Message</label>
 		<textarea name="texte" id="inputTexte" class="form-control" rows="3">
 		</textarea>
-	</div>
+	
 
 	<button type="submit" class="btn btn-primary">Submit</button>
 </form>

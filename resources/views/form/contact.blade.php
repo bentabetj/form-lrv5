6fb7b26301e10fa4c156e8ef447ab8707208aeeb
BentabetAbdelDjalil <bentabetj@yahoo.fr>
2016-05-21 13:54:22 +0100

    ajout input de crsf

--- resources/views/form/contact.blade.php
+++ resources/views/form/contact.blade.php
@@ -8,14 +8,14 @@
 		<label for="nom">Nom</label></br>
 		{{ $errors->first('nom', '<small class="help-block">:message</small>') }}
 	</div>	
-	    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
 		<input type="text" name="nom" class="form-control" id="nom">
 		<label for="email">Mail</label></br>
 		<input type="mail" name="email" class="form-control" id="email"></br>
 		<label for="inputTexte">Message</label>
 		<textarea name="texte" id="inputTexte" class="form-control" rows="3">
 		</textarea>
-	
+	    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 	<button type="submit" class="btn btn-primary">Submit</button>
 </form>
